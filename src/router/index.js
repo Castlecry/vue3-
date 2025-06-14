79188be4e83409feb8ca82a9b4d77fdd202a7fca
Castlecry <3555670675@qq.com>
2025-06-14 19:37:07 +0800

    复原2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { useUserStore } from '@/store/modules/user'
+import { useUserStore } from '@/stores/user'
 import { ElMessage } from 'element-plus'
 
 const router = createRouter({
@@ -92,14 +92,14 @@ const router = createRouter({
 export default router
 
 // 路由前置守卫
-router.beforeEach((to) => {
-  const userStore = useUserStore()
-  //如果没有token，且访问非登录页，就跳转回来
-  if (!userStore.token && to.path !== '/login') {
-    ElMessage({
-      message: '登录状态过期，请先登录',
-      type: 'error'
-    })
-    router.push('/login')
-  }
-})
+// router.beforeEach((to) => {
+//   const userStore = useUserStore()
+//   //如果没有token，且访问非登录页，就跳转回来
+//   if (!userStore.token && to.path !== '/login') {
+//     ElMessage({
+//       message: '登录状态过期，请先登录',
+//       type: 'error'
+//     })
+//     router.push('/login')
+//   }
+// })
